12006adb0ac4d270d587016feef5c17426ab54e7
James <67306283+CYXup6@users.noreply.github.com>
2021-10-21 21:53:17 +0800

    Update data_transform.py

--- data_transform/data_transform.py
+++ data_transform/data_transform.py
@@ -3,13 +3,33 @@ import csv
 import glob
 import json
 from tqdm import tqdm
+import spacy
+from spacy.matcher import Matcher
+from keybert import KeyBERT
+
+#下載pretrained model指令 >> python -m spacy download en_core_web_trf 
+
+##########################
+#   載入並設定model參數   #
+##########################
+nlp = spacy.load('en_core_web_trf') #load spacy pretrained model 
+patterns = [
+    [{"DEP": "compound","OP": "*"}, {"POS": "NOUN"}],
+    [{"DEP": "compound","OP": "*"}, {"POS": "PROPN"}],
+
+    ]
+matcher = Matcher(nlp.vocab)
+matcher.add("NOUN", [patterns[0]])
+matcher.add("PROPN", [patterns[1]])
+model = KeyBERT(model='paraphrase-MiniLM-L6-v2') #load keybert pretrained model 
+
 
 ##########################
 # 將metadata跟逐字稿整合 #
 ##########################
 all_transcript = {} #以每集的episodeID作為key值、內容為整集逐字稿 的dictionary
 all_keywords = {} #以每集的episodeID作為key值、內容為各段keywords 的dictionary
-for filename in tqdm(glob.iglob(r'/path/to/spotify-podcasts-2020/*/*/*/*/*.json', recursive=True)):
+for filename in tqdm(glob.iglob(r'spotify-podcasts-2020/*/7/*/*/*.json', recursive=True)):
 	f = open(filename)
 	f = json.load(f)
 	
@@ -35,8 +55,19 @@ for filename in tqdm(glob.iglob(r'/path/to/spotify-podcasts-2020/*/*/*/*/*.json'
 			transcript=transcript+tmp+' '
 			
 			#從每段的文字中抓關鍵字
-			#keywords[starttime] = extract(tmp)
-			#待晉毅補充
+			doc = nlp(tmp)
+			matches = matcher(doc) 
+			extracted_keywords=[]  #此段文字稿抓到的所有關鍵字(keyBERT extraction + spaCy NER)
+
+			result=[]
+			for match_id, start, end in matches:
+			    span = doc[start:end]
+			    result.append(span.text.lower())
+			result=list(set(result))  #用PoS 取得複合名詞/名詞片語...
+			extracted_keywords = extracted_keywords + model.extract_keywords(tmp, candidates=result, top_n=10)  #用bert抓關鍵字
+			extracted_keywords = extracted_keywords + list(set([(ee.text.lower(),0) for ee in doc.ents if ee.label_ == 'PERSON' or ee.label_ == 'ORG' or ee.label_ == 'GPE'])) #用spacy NER抓命名實體 
+
+			keywords[starttime] = extracted_keywords
 
 			#reset tmp + set starttime to another segment
 			tmp=word['word']+' '
@@ -45,7 +76,6 @@ for filename in tqdm(glob.iglob(r'/path/to/spotify-podcasts-2020/*/*/*/*/*.json'
 	#把此集的分段關鍵字、整個逐字稿以episodeID為key值暫存起來
 	all_keywords[Id] = keywords
 	all_transcript[Id] = transcript
-
 print('done')
 
 
@@ -88,4 +118,3 @@ with open('/path/to/only7.json') as f:
 	
 	#insert進去es
 	helpers.bulk(es, insert)
-
